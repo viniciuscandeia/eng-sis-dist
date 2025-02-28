4e50ab7f89a698ca23fca60ea3f5156d1baaa365
Vinicius Candeia <viniciuscandeia12@gmail.com>
2025-02-28 14:48:18 -0300

    New commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Olá Mundo Flask com Testes!"
+    return 'Olá, Mundo DevOps com Flask!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,16 @@
 import unittest
-import app  # Importa o módulo app.py (nossa aplicação Flask)
+from app import app  # Importa a sua aplicação Flask
 
 class TestApp(unittest.TestCase):
 
     def setUp(self):
-        self.app = app.app.test_client() # Cria um cliente de teste do Flask
+        self.app = app.test_client()
         self.app.testing = True
 
     def test_index_route(self):
-        response = self.app.get('/') # Faz uma requisição GET para a rota '/'
-        self.assertEqual(response.status_code, 200) # Verifica se o status code é 200 (OK)
-        self.assertEqual(response.data.decode('utf-8'), "Olá Mundo Flask com Testes!") # Verifica se o conteúdo da resposta é o esperado
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), 'Olá, Mundo DevOps com Flask!')
 
 if __name__ == '__main__':
     unittest.main()
